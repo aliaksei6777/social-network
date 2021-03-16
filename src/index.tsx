ffd85cbb2a9b7a3caaf2929651f9bd10ef2faa0e
aliaksei6777 <aliaksei6777@gmail.com>
2021-03-16 18:58:31 +0300

    before redux+(reportWebVitals() commented)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import store from "./redux/state";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
@@ -23,4 +23,4 @@ store.subscribe(rerenderTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
